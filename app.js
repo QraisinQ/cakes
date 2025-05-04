a8599990b76c62ec016632061f3ec19c9ef1a27a
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-05-04 15:25:30 +0100

    Add get cakes from database on the index page

--- app.js
+++ app.js
@@ -9,40 +9,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Set view engine
 app.set('view engine', 'ejs');
 
-// //Connect to the database
-// connection.connect((err) => {
-// 	if (err) {
-// 		console.error('Error connection to database: ', err);
-// 	} else {
-// 		console.log('Connected to database!');
-// 	}
+app.get('/', (req, res) => {
+	myDatabase
+		.getAllCakes()
+		.then(([rows, fields]) => {
+			res.render('index', { cakes: rows });
+		})
+		.catch((err) => console.log(err));
+});
 
 app.get('/shop', function (req, res) {
-	myDatabase.getAllCakes().then(([rows, fields]) => {
-		res.send(rows);
-	});
 	// console.log(myDatabase.getAllCakes());
 });
 
 //Server static files from the public ddirectory
 app.use(express.static('public'));
-
-// //route to handle the login form submission
-// app.post('/login', function (req, res) {
-// 	const username = req.body.username;
-// 	const password = req.body.password;
-// 	// const { username, password = req, body }; // the same as previous 2 lines
-// 	const authenticated = auth.authenticateUser(username, password);
-// 	console.log(authenticated);
-
-// 	if (authenticated) {
-// 		console.log('Authentiction was successbul!');
-// 		res.render('index');
-// 	} else {
-// 		console.log('Authentication was NOT successful!');
-// 		res.render('login');
-// 	}
-// });
 
 //Start the server
 app.listen(PORT, () => {
