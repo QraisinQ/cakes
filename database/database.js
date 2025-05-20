28526c1f95d25ce7ef3a6be115edeef18cd4058c
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-05-20 14:16:46 +0100

    Add comments to database.js, styles.css

--- database/database.js
+++ database/database.js
@@ -1,30 +1,38 @@
 //Connect ot database:
-const mysql = require('mysql2');
+
+// Import mysql2 library
+const mysql = require("mysql2");
 
 class MyDatabase {
-	constructor() {
-		//Create a connection to MySQL database
-		this.connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: 'root',
-			database: 'g00472936',
-		});
-	}
-
-	getAllCakes() {
-		return this.connection.promise().query('SELECT * FROM cakes;');
-	}
-
-	getCakesByIdList(idList) {
-		return this.connection.promise().query(`SELECT * FROM cakes WHERE product_id IN (${idList.join(',')});`);
-	}
-
-	getUserByEmail(email) {
-		return this.connection.promise().query(`SELECT * FROM users WHERE email='${email}';`);
-	}
+  constructor() {
+    //Create a connection to MySQL database
+    this.connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "g00472936",
+    });
+  }
+  // Retrieves all from cakes table
+  getAllCakes() {
+    return this.connection.promise().query("SELECT * FROM cakes;");
+  }
+  // REtrieves all records in range
+  getCakesByIdList(idList) {
+    return this.connection
+      .promise()
+      .query(`SELECT * FROM cakes WHERE product_id IN (${idList.join(",")});`);
+  }
+  // Retrieves a user from database whose email matches the input
+  getUserByEmail(email) {
+    return this.connection
+      .promise()
+      .query(`SELECT * FROM users WHERE email='${email}';`);
+  }
 }
 
+//Creates instance of MyDatabase
 const myDatabase = new MyDatabase();
 
+// Exports module
 exports.myDatabase = myDatabase;
